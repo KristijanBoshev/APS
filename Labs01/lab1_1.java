33d95bd8929c4a0db7d3d879677cc8a4f9d0f4e8
KristijanBoshev <154442308+KristijanBoshev@users.noreply.github.com>
2024-03-26 21:23:52 +0100

    Add Labs

---
+++ Labs01/lab1_1.java
@@ -0,0 +1,40 @@
+// Lab 1.1
+
+// For a given array of random numbers given from standard input,
+// perform a shift of all zeros at the beginning of the sequence.
+// Print the transformed array to standard output.
+
+import java.util.Scanner;
+
+public class PushZeroes {
+    static void pushZeroesToBeginning(int[] arr, int n) {
+        int ct = n - 1;
+
+        for(int i = n - 1; i >= 0; i--) {
+            if(arr[i] != 0) {
+                arr[ct--] = arr[i];
+            }
+        }
+
+        while(ct >= 0) {
+            arr[ct--] = 0;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int[] arr = new int[n];
+
+        for(int i = 0; i < n; i++) {
+            arr[i] = in.nextInt();
+        }
+
+        pushZeroesToBeginning(arr, n);
+
+        System.out.print("Transformiranata niza e:\n");
+        for(int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
---
+++ Labs01/lab1_2.java
@@ -0,0 +1,28 @@
+// Lab 1.2
+
+// For a given word entered from standard input, print it reversed.
+// On input in the first line, the number of words that will be entered is given.
+// In the following lines, the words are entered.
+
+import java.util.Scanner;
+
+public class ReverseWord {
+    public static void printReversed(String word) {
+        for(int i = word.length() - 1; i >= 0; i--) 
+            System.out.print(word.charAt(i));
+        }
+        System.out.print("\n");
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int n = in.nextInt();
+        in.nextLine();
+
+        for(int i = 0; i < n; i++) {
+            String str = in.nextLine();
+            printReversed(str);
+        }
+    }
+}
